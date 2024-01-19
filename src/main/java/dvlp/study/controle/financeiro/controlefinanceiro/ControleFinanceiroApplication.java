bab84ef8095d81a164e2959e77b6a2a1b1487ba5
Erick Silvestre <erickaguiarsilvestre@gmail.com>
2024-01-19 15:24:01 -0300

    first-commit

---
+++ src/main/java/dvlp/study/controle/financeiro/controlefinanceiro/ControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package dvlp.study.controle.financeiro.controlefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroApplication.class, args);
+	}
+
+}
